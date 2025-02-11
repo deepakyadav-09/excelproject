bf1ef82da418df3b303f919543f12c564e0c2558
deepakyadav-09 <deepak.yadav@elookinto.org>
2025-02-11 12:34:07 +0530

    formate app.js file

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const PORT = 4009;
 const express = require('express');
 const app = express();
 const { excelRoutes } = require('./routes/excelRoutes');
-app.get('/', (req, res) =>{
-    res.send('this is by deepak and testing of ngrok')
-})
 app.use(express.urlencoded({ extended: true }));
 app.use('/excel', excelRoutes);
+
+/*
+// this is for check of ngrok
+app.get('/', (req, res) =>{
+    res.send('this is by deepak and testing of ngrok')
+})*/
 app.listen(PORT, () => console.log('Server is SUCESSFULLY running on PORT', PORT));
